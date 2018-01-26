c6dd159452def2e9baf41ff374623707c6489bd3
dagi12 <dagi12@o2.pl>
2018-01-26 09:31:03 +0100

    #003690 daggermock test di, line chart test

--- src/main/java/pl/edu/amu/wmi/erykandroidcommon/di/TestComponent.kt
+++
@@ -1,8 +0,0 @@
-//package pl.edu.amu.wmi.erykandroidcommon.di
-//
-//import dagger.Component
-//import javax.inject.Singleton
-//
-//@Singleton
-//@Component(modules = arrayOf(TestModule::class))
-//interface TestComponent
